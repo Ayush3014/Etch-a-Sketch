68fcdb11296fe01e7e4c14d3a4d9353c622b1834
Ayush <ayushpanchal005@gmail.com>
2023-06-07 23:01:37 +0530

    Added random rgb colors and black color selection using dropdown menu

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 const item = document.querySelectorAll('.item');
 const reset = document.querySelector('.reset');
+const black = document.getElementById('black');
+const random = document.getElementById('random');
+const selected = document.getElementById('dropdown');
 
 item.forEach(element => {
     element.addEventListener('mouseenter', ()=>{
-        if(element.style.backgroundColor == "black")
-        {
-            element.style.backgroundColor = "white";
-        }
-        else
-        {
-            element.style.backgroundColor = "black";
-        }
+        element.style.backgroundColor = "black";
     });    
 });
 
-reset.addEventListener('click', ()=>{
+reset.addEventListener('click', () => {
     item.forEach(element => {
         element.style.backgroundColor = "white";
     })
 });
+
+selected.addEventListener("change", () => {
+    if(selected.value === black.value)
+    {
+        item.forEach(element => {
+            element.addEventListener('mouseenter', ()=>{
+                element.style.backgroundColor = "black";
+            });    
+        });
+    }
+
+    else if(selected.value === random.value)
+    {
+        item.forEach(element => {
+            element.addEventListener('mouseenter', () => {
+                const red = Math.floor(Math.random() * 256);
+                const blue = Math.floor(Math.random() * 256);
+                const green = Math.floor(Math.random() * 256);
+
+                element.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
+            });
+        });
+    }
+});
+
